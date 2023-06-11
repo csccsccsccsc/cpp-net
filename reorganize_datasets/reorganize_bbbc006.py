476c31a60744a83358d634c0b4877d1eed7c7bea
csccsccsccsc <q490422704@qq.com>
2023-06-11 17:52:47 +0800

    Update reorganize_bbbc006.py

--- reorganize_datasets/reorganize_bbbc006.py
+++ reorganize_datasets/reorganize_bbbc006.py
@@ -6,61 +6,90 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 import tqdm
+import shutil
 
-#######
-### Download PanNuke from https://data.broadinstitute.org/bbbc/BBBC006 (In CPP-Net, we only use the "BBBC006_v1_images_z_16" images.)
+img_dir = './BBBC006_v1_images_z_16'
+msk_dir = './BBBC006_v1_labels'
+save_img_dir = './reorganize_dataset'
 
-def image_matching(base_name, name_list):
-    match_name = []
-    for name in name_list:
-        if name.find(base_name)!=-1:
-            match_name.append(name)
-    return match_name
+if not os.path.exists(save_img_dir):
+    os.makedirs(save_img_dir)    
 
-img_dir = 'DATA ROOT PATH/bbbc006/BBBC006/BBBC006_v1_images_z_16'
-msk_dir = 'DATA ROOT PATH/bbbc006/BBBC006_v1_labels'
-save_img_dir = 'DATA ROOT PATH/bbbc006/BBBC006_split'
-name_list = sorted(os.listdir(os.path.join(msk_dir)))
 img_name_list = os.listdir(os.path.join(img_dir))
 
-rng = np.random.RandomState(42)
-ind = rng.permutation(len(name_list))
-n_val = max(1, int(round(0.15 * len(ind))))
-n_test = max(1, int(round(0.15 * len(ind))))
-n_train = len(ind) - n_val - n_test
-ind_train, ind_val, ind_test = ind[:n_train], ind[n_train:n_train+n_val], ind[n_train+n_val:]
-print('number of images: %3d' % len(name_list))
-print('- training:       %3d' % len(ind_train))
-print('- validation:     %3d' % len(ind_val))
-print('- testing:        %3d' % len(ind_test))
+name_list = []
+msk_loc_list = {}
+img_loc_list_w1 = {}
+img_loc_list_w2 = {}
+for name in img_name_list:
+    name_terms = name.split('_')
+    if name_terms[3][0:2] == 'w1':
+        name_list.append('_'.join(name_terms[0:3]))
+        img_loc_list_w1.update({'_'.join(name_terms[0:3]) : os.path.join(img_dir, name)})
+        msk_loc_list.update({'_'.join(name_terms[0:3]) : os.path.join(msk_dir, '_'.join(name_terms[0:3])+'.png')})
+    else:
+        img_loc_list_w2.update({'_'.join(name_terms[0:3]) : os.path.join(img_dir, name)})
 
-inds = {'train':ind_train, 'val':ind_val, 'test':ind_test}
+# split training, validation, and testing
+# Note that each 'w1' and 'w2' image pair of ths same image is groupedd into the same dataset.
+n_img = len(name_list)
+rngstate = np.random.RandomState(42)
+testset = rngstate.choice(n_img, int(0.2*n_img), replace=False)
+trainvalset = np.setdiff1d(np.arange(n_img), testset)
+valset = rngstate.choice(trainvalset, int(0.2*n_img), replace=False)
+trainset = np.setdiff1d(trainvalset, valset)
+print('total : ', 2*n_img)
+print('train : ', 2*len(trainset))
+print('val : ', 2*len(valset))
+print('test : ', 2*len(testset))
 
-for split_flag in ['train', 'val', 'test']:
-    if not(os.path.exists(os.path.join(save_img_dir, split_flag, 'images'))):
-        os.makedirs(os.path.join(save_img_dir, split_flag, 'images'))
-    if not(os.path.exists(os.path.join(save_img_dir, split_flag, 'masks'))):
-        os.makedirs(os.path.join(save_img_dir, split_flag, 'masks'))
-    image_id = 0
-    with open(os.path.join(save_img_dir, split_flag+'_name.txt'), 'w') as f:
-        cur_ind = inds[split_flag]
-        bar = tqdm.tqdm(range(len(cur_ind)))
-        for i_name in bar:
-            name = name_list[cur_ind[i_name]]
-            mask_source = os.path.join(msk_dir, name)
-            mask = io.imread(mask_source)
-            base_name = '.'.join(name.split('.')[:-1])
-            imgs_source = image_matching(base_name, img_name_list)
-            count_match_img = len(imgs_source)
-            for img_source in imgs_source:
-                img = io.imread(os.path.join(img_dir, img_source))
-                img_target = os.path.join(save_img_dir, split_flag, 'images', str(image_id)+'.tif')
-                io.imsave(img_target, img)
-                mask_target = os.path.join(save_img_dir, split_flag, 'masks', str(image_id)+'.tif')
-                io.imsave(mask_target, mask)
-                #print(image_id, img.dtype, mask.dtype, img.shape, mask.shape)
-                image_id += 1
+dataset_idxlist = {'val':valset, 'test':testset, 'train':trainset}
+for dataset_type in ['train', 'val', 'test']:
+    cur_save_dir = os.path.join(save_img_dir, dataset_type)
+    idxlist = dataset_idxlist[dataset_type]
 
-                # Filename record
-                f.write(str(image_id)+','+name+','+split_flag+'\n')
-                bar.set_description("Processing {:d} | {:d}...image base name: {}".format(image_id, 2*len(inds[split_flag]), base_name))
+    print('Processing '+dataset_type+' dataset (including '+str(2*len(idxlist))+' images')
+    print(cur_save_dir)
+
+
+    if not os.path.exists(os.path.join(cur_save_dir, 'w1_images')):
+        os.makedirs(os.path.join(cur_save_dir, 'w1_images'))
+    if not os.path.exists(os.path.join(cur_save_dir, 'w1_masks')):
+        os.makedirs(os.path.join(cur_save_dir, 'w1_masks'))
+    if not os.path.exists(os.path.join(cur_save_dir, 'w2_images')):
+        os.makedirs(os.path.join(cur_save_dir, 'w2_images'))
+    if not os.path.exists(os.path.join(cur_save_dir, 'w2_masks')):
+        os.makedirs(os.path.join(cur_save_dir, 'w2_masks'))
+
+
+    processed_list = []
+    processed_idx_list = []
+    f_img = open(os.path.join(cur_save_dir, dataset_type + '_images_name_list.txt'), 'w')
+    f_msk = open(os.path.join(cur_save_dir, dataset_type + '_masks_name_list.txt'), 'w')
+    for idx in idxlist:
+        f_img.write(os.path.join(cur_save_dir, 'w1_images', name_list[idx]+'_w1.png') + '\n')
+        f_msk.write(os.path.join(cur_save_dir, 'w1_masks', name_list[idx]+'_w1.png') + '\n')
+        f_img.write(os.path.join(cur_save_dir, 'w2_images', name_list[idx]+'_w2.png') + '\n')
+        f_msk.write(os.path.join(cur_save_dir, 'w2_masks', name_list[idx]+'_w2.png') + '\n')
+
+
+        if not( os.path.join(cur_save_dir, 'w1_images', name_list[idx]+'_w1.png') in processed_list ):
+            processed_list.append(os.path.join(cur_save_dir, 'w1_images', name_list[idx]+'_w1.png'))
+            processed_idx_list.append(idx)
+        else:
+            print(os.path.join(cur_save_dir, 'w1_images', name_list[idx]+'_w1.png'))
+
+
+        if not( os.path.join(cur_save_dir, 'w2_images', name_list[idx]+'_w2.png') in processed_list ):
+            processed_list.append(os.path.join(cur_save_dir, 'w2_images', name_list[idx]+'_w2.png'))
+        else:
+            print(idx, os.path.join(cur_save_dir, 'w2_images', name_list[idx]+'_w2.png'))
+            for pi, p in zip(processed_idx_list, processed_list):
+                print(pi, p)
+            assert(False)
+
+        # NOTE: the original images are '.tif' files...
+        shutil.copy(img_loc_list_w1[name_list[idx]], os.path.join(cur_save_dir, 'w1_images', name_list[idx]+'_w1.png'))
+        shutil.copy(img_loc_list_w2[name_list[idx]], os.path.join(cur_save_dir, 'w2_images', name_list[idx]+'_w2.png'))
+        shutil.copy(msk_loc_list[name_list[idx]], os.path.join(cur_save_dir, 'w1_masks', name_list[idx]+'_w1.png'))
+        shutil.copy(msk_loc_list[name_list[idx]], os.path.join(cur_save_dir, 'w2_masks', name_list[idx]+'_w2.png'))
